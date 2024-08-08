cba194ae3bf66c2e3e0900e313ed7b4b535f00db
GinterZX <ginterzh@gmail.com>
2024-08-08 23:04:37 +0500

    Исправлен импорт с import на require

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import SxGeo from '../dist/sxgeo.js';
+const SxGeo = require('../dist/sxgeo').default;
 
 const geoInstance = new SxGeo('test/SxGeoCity.dat', 1);
 const result = geoInstance.getCityFull('1.1.1.1');
